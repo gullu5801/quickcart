e543bfedbbbcc41754f24bf91f65f93490a94891
Gullu5801 <golupat77@gmail.com>
2026-03-08 20:47:23 +0530

    part 3 header fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,39 @@
 import { Link } from "react-router-dom";
 import { useCart } from "../hooks/useCart";
-import "../styles/Header.css"
+import "../styles/Header.css";
 
 function Header({ searchTerm, onSearchChange }) {
 
   const { getTotalItems, toggleCart } = useCart();
 
   return (
-    <header>
+    <header className="header">
 
-      <Link to="/">
-        <h1>🛒 QuickCart</h1>
-      </Link>
+      <div className="header-container">
 
-      <input
-        type="text"
-        placeholder="Search products..."
-        value={searchTerm}
-        onChange={(e) => onSearchChange(e.target.value)}
-      />
+        {/* Logo */}
+        <Link to="/" className="logo">
+          🛒 <span>QuickCart</span>
+        </Link>
 
-      <button onClick={toggleCart}>
-        Cart ({getTotalItems()})
-      </button>
+        {/* Search */}
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search products..."
+          value={searchTerm}
+          onChange={(e) => onSearchChange(e.target.value)}
+        />
+
+        {/* Cart Icon */}
+        <button className="cart-btn" onClick={toggleCart}>
+          🛒
+          <span className="cart-count">
+            {getTotalItems()}
+          </span>
+        </button>
+
+      </div>
 
     </header>
   );
